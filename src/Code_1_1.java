abdf420397d98ab17f5d2d32626e22109d8b2cb2
llin <dev.linxu@gmail.com>
2016-02-23 23:08:40 -0500

    code 1_1

---
+++ src/Code_1_1.java
@@ -0,0 +1,22 @@
+/**
+ * Created by llin on 2016-02-23.
+ */
+public class Code_1_1 {
+
+    public static void main(String[] args){
+        System.out.println(isUniqueChars2("nima"));
+    }
+
+    public static boolean isUniqueChars2(String str){
+        if (str.length() > 256) return false;
+        boolean[] char_set = new boolean[256];
+        for(int i = 0; i < str.length(); i++){
+            int val = str.charAt(i);
+            if(char_set[val]){
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
+    }
+}

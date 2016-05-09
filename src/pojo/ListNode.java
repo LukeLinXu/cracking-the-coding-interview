db7780ee067062d5f41e507d5e2456490fe05916
unknown <llin@DEV-C601.MSI.com>
2016-05-09 10:53:04 -0400

    add ListNode

---
+++ src/pojo/ListNode.java
@@ -0,0 +1,15 @@
+package pojo;
+
+/**
+ * Created by llin on 02/03/2016.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}

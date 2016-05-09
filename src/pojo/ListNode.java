a4975016262fd9aeecb7fb4506213a58e360d102
unknown <llin@DEV-C601.MSI.com>
2016-05-09 17:30:49 -0400

    edit ListNode

--- src/pojo/ListNode.java
+++ src/pojo/ListNode.java
@@ -7,9 +7,8 @@ public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode(int val, ListNode next) {
+    public ListNode(int val) {
         this.val = val;
-        this.next = next;
     }
 
 }

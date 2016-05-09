2ebf6a4821bc3407e9f5bddcdbd3876d91e1b047
llin <dev.linxu@gmail.com>
2016-05-08 23:26:23 -0400

    add tree node

---
+++ src/pojo/TreeNode.java
@@ -0,0 +1,12 @@
+package pojo;
+
+/**
+ * Created by llin on 2016-05-08.
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
